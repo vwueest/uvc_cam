aac6fad920887b4b2ab19fecd152c7f53a263cf8
Valentin Wueest <valentinwueest@gmail.com>
2019-10-21 12:03:20 +0200

    added more params as dynamic & changed init to bus ID instead of prod ID

--- src/uvc_cam_node.py
+++ src/uvc_cam_node.py
@@ -29,26 +29,36 @@ class UVCCamNode:
 
         rospy.init_node('{}_node'.format(DEFAULT_CAMERA_NAME), argv=sys.argv)
 
-        self.image_topic  = rospy.get_param('~image', DEFAULT_IMAGE_TOPIC)
-        self.camera_topic = rospy.get_param('~camera', DEFAULT_CAMERA_TOPIC)
-        self.calibration  = rospy.get_param('~calibration', '')
-        self.encoding     = rospy.get_param('~encoding','mono8')
-        self.width        = rospy.get_param('~width', DEFAULT_WIDTH)
-        self.height       = rospy.get_param('~height', DEFAULT_HEIGHT)
-        self.fps          = rospy.get_param('~fps', DEFAULT_FPS)
-	self.contrast     = rospy.get_param('~contrast', 0)
-	self.sharpness    = rospy.get_param('~sharpness', 1)
-        self.cam_prod_id  = rospy.get_param('~cam_prod_id', 9760)
-        self.gamma        = rospy.get_param('~gamma',80)
-        self.exposure_abs = rospy.get_param('~exposure',50) #50 - 
-        self.brightness   = rospy.get_param('~brightness',1)
+        self.image_topic    = rospy.get_param('~image', DEFAULT_IMAGE_TOPIC)
+        self.camera_topic   = rospy.get_param('~camera', DEFAULT_CAMERA_TOPIC)
+        self.calibration    = rospy.get_param('~calibration', '')
+        self.encoding       = rospy.get_param('~encoding','mono8')
+        self.width          = rospy.get_param('~width', DEFAULT_WIDTH)
+        self.height         = rospy.get_param('~height', DEFAULT_HEIGHT)
+        self.fps            = rospy.get_param('~fps', DEFAULT_FPS)
+        self.cam_prod_id    = rospy.get_param('~cam_prod_id', 9760)
+
+	self.contrast       = rospy.get_param('~contrast', 0)
+	self.sharpness      = rospy.get_param('~sharpness', 1)
+        self.gamma          = rospy.get_param('~gamma',80)
+        self.exposure_abs   = rospy.get_param('~exposure',50) #50 - 
+        self.brightness     = rospy.get_param('~brightness',1)
+        self.hue            = rospy.get_param('~hue',0)
+        self.saturation     = rospy.get_param('~saturation',0)
+        self.pwr_line_freq  = rospy.get_param('~pwr_line_freq',1)
         self.backlight_comp = rospy.get_param('~backlight_comp',0)
+        self.auto_exposure  = rospy.get_param('~auto_exposure',8)
+        self.auto_exp_prio  = rospy.get_param('~auto_exp_prio',0)
+        self.white_bal_auto = rospy.get_param('~white_bal_auto',True)
+        self.white_bal      = rospy.get_param('~white_bal',4600)
 
 	# possible values to set can be found by running "rosrun uvc_camera uvc_camera_node _device:=/dev/video0", see http://github.com/ros-drivers/camera_umd.git
 	dev_list = uvc.device_list()
 	for i in range(0,len(dev_list)):
-          rospy.loginfo('available device %i: idProd: %i'%(i,dev_list[i]["idProduct"]))
-	  if dev_list[i]["idProduct"] == self.cam_prod_id:
+	  print dev_list[i]
+          rospy.loginfo('available device %i: idProd: %i   -   comparing to idProd: %i'%(i,int(dev_list[i]["idProduct"]),self.cam_prod_id))
+	  if i == self.cam_prod_id: #int(dev_list[i]["idProduct"]) == self.cam_prod_id:
+            rospy.loginfo("connecting to camera idProd: %i, device %i"%(self.cam_prod_id,i))
             self.cap = uvc.Capture(dev_list[i]["uid"])
             #self.cap.set(CV_CAP_PROP_CONVERT_RGB, false)
             rospy.loginfo("successfully connected to camera %i"%i)
@@ -58,16 +68,17 @@ class UVCCamNode:
 	self.controls_dict = dict([(c.display_name, c) for c in self.cap.controls])
 	self.controls_dict['Brightness'].value = self.brightness #10 #[-64,64], not 0 (no effect)!!
 	self.controls_dict['Contrast'].value = self.contrast #0 #[0,95]
-	self.controls_dict['Hue'].value = 0 #[-2000,2000]
-	self.controls_dict['Saturation'].value = 0 #[0,100]
+	self.controls_dict['Hue'].value = self.hue #[-2000,2000]
+	self.controls_dict['Saturation'].value = self.saturation #[0,100]
 	self.controls_dict['Sharpness'].value = self.sharpness #1 #[1,100]
 	self.controls_dict['Gamma'].value = self.gamma #[80,300]
-	self.controls_dict['Power Line frequency'].value = 1 #1:50Hz, 2:60Hz
-	self.controls_dict['Backlight Compensation'].value = False #True or False
+	self.controls_dict['Power Line frequency'].value = self.pwr_line_freq #1:50Hz, 2:60Hz
+	self.controls_dict['Backlight Compensation'].value = self.backlight_comp #True or False
 	self.controls_dict['Absolute Exposure Time'].value = self.exposure_abs #[78,10000] set Auto Exposure Mode to 1
-	self.controls_dict['Auto Exposure Mode'].value = 1 #1:manual, 8:apperturePriority
-	self.controls_dict['White Balance temperature,Auto'].value = True
-	#self.controls_dict['White Balance temperature'].value = 4600 #[2800,6500]
+	self.controls_dict['Auto Exposure Mode'].value = self.auto_exposure #1:manual, 8:apperturePriority
+	self.controls_dict['Auto Exposure Priority'].value = self.auto_exp_prio #1:manual, 8:apperturePriority
+	self.controls_dict['White Balance temperature,Auto'].value = self.white_bal_auto
+	self.controls_dict['White Balance temperature'].value = self.white_bal #[2800,6500]
 	rospy.loginfo("These camera settings will be applied:")
 	for c in self.cap.controls:
 		rospy.loginfo('%s: %i'%(c.display_name, c.value))
@@ -94,7 +105,13 @@ class UVCCamNode:
           {backlight_comp}, \
           {gamma}, \
           {contrast}, \
-          {sharpness}
+          {sharpness}, \
+          {hue} \
+          {saturation} \
+          {pwr_line_freq} \
+          {auto_exp_prio} \
+          {white_bal_auto} \
+          {white_bal} \
           """.format(**config))
         self.controls_dict['Absolute Exposure Time'].value = config.exposure;
         self.controls_dict['Auto Exposure Mode'].value = config.auto_exposure;
@@ -103,6 +120,12 @@ class UVCCamNode:
         self.controls_dict['Gamma'].value = config.gamma;
         self.controls_dict['Contrast'].value = config.contrast;
         self.controls_dict['Sharpness'].value = config.sharpness;
+        self.controls_dict['Hue'].value = config.hue;
+        self.controls_dict['Saturation'].value = config.saturation;
+        self.controls_dict['Power Line frequency'].value = config.pwr_line_freq;
+        self.controls_dict['Auto Exposure Priority'].value = config.auto_exp_prio;
+        self.controls_dict['White Balance temperature,Auto'].value = config.white_bal_auto;
+        self.controls_dict['White Balance temperature'].value = config.white_bal;
 
 	for c in self.cap.controls:
 		rospy.loginfo('%s: %i'%(c.display_name, c.value))
